60938084d8e130c4e6ee4d0f4e1751db15de32ff
Rubasoff <denisufaltc@gmail.com>
2016-11-02 14:18:07 +0500

    Init DB

---
+++ src/main/java/ru/karachurin/restaurants/Application.java
@@ -0,0 +1,14 @@
+package ru.karachurin.restaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Денис on 02.11.2016.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}

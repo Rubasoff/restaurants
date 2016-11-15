b256ff48ab8aab27fbab803533519c25f05222ed
Rubasoff <denisufaltc@gmail.com>
2016-11-15 09:35:36 +0500

    User class revision

---
+++ src/main/java/ru/karachurin/restaurants/model/Role.java
@@ -0,0 +1,9 @@
+package ru.karachurin.restaurants.model;
+
+/**
+ * Created by Денис on 15.11.2016.
+ */
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN;
+}
--- src/main/java/ru/karachurin/restaurants/model/User.java
+++ src/main/java/ru/karachurin/restaurants/model/User.java
@@ -1,14 +1,16 @@
 package ru.karachurin.restaurants.model;
 
+import org.hibernate.annotations.*;
+import org.hibernate.annotations.Cache;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
-import javax.persistence.Column;
+import javax.persistence.*;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
 import java.util.Date;
+import java.util.Set;
 
 /**
  * Created by Денис on 09.11.2016.
@@ -33,4 +35,75 @@ public class User extends NamedEntity {
 
     @Column(name = "registered", columnDefinition = "timestamp default now()")
     private Date registered = new Date();
+
+    @Enumerated(EnumType.STRING)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    @ElementCollection(fetch = FetchType.EAGER)
+    @BatchSize(size = 200)
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+    private Set<Role> roles;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String email, String password, boolean enabled, Date registered, Set<Role> roles) {
+        super(id, name);
+        this.email = email;
+        this.password = password;
+        this.enabled = enabled;
+        this.registered = registered;
+        this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public Date getRegistered() {
+        return registered;
+    }
+
+    public void setRegistered(Date registered) {
+        this.registered = registered;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", enabled=" + enabled +
+                ", registered=" + registered +
+                ", roles=" + roles +
+                '}';
+    }
 }
